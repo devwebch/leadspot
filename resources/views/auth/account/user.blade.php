77d81c7609d27c01803861f4ae88aad6db749791
Simon Rapin <srapin@novahealthsa.com>
2016-09-05 17:01:55 +0200

    - user account page need to be redone

--- resources/views/auth/account/user.blade.php
+++ resources/views/auth/account/user.blade.php
@@ -1,50 +1,82 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: SIMON
- * Date: 10.08.2016
- * Time: 15:17
- */
+@extends('layouts.app')
 
-$logged_in          = false;
-$user_first_name    = '';
-$user_last_name     = '';
+@section('title', 'Account')
 
-if (Auth::check()) {
-    $logged_in  = true;
-    $user_first_name    = Auth::user()->first_name;
-    $user_last_name     = Auth::user()->last_name;
-} else {
+@section('styles')
+@endsection
 
-}
-?>
+@section('scripts')
+@endsection
 
-@if($logged_in)
-<!-- START User Info-->
-<div class="visible-lg visible-md m-t-10">
-    <div class="pull-left p-r-10 p-t-10 fs-16 font-heading">
-        <span class="semi-bold">{{$user_first_name}}</span>
-        <span class="text-master">{{$user_last_name}}</span>
+@section('content')
+    <div class="row">
+        <div class="col-md-12 m-b-10">
+            <div class="panel">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-6">
+
+                            <h2>My account</h2>
+
+                            <div class="panel panel-transparent">
+                                <ul class="nav nav-tabs nav-tabs-simple nav-tabs-left bg-white" id="tab-3">
+                                    <li class="active">
+                                        <a data-toggle="tab" href="#tab3hellowWorld">My informations</a>
+                                    </li>
+                                    <li>
+                                        <a data-toggle="tab" href="#tab3FollowUs">Subscription</a>
+                                    </li>
+                                    <li>
+                                        <a data-toggle="tab" href="#tab3Inspire">Misc.</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content bg-white">
+                                    <div class="tab-pane" id="tab3hellowWorld">
+                                        <div class="row column-seperation">
+                                            <div class="col-md-6">
+                                                <h3>
+                                                    <span class="semi-bold">Sometimes </span>Small things in life
+                                                    means the most
+                                                </h3>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <h3 class="semi-bold">
+                                                    great tabs
+                                                </h3>
+                                                <p>Native boostrap tabs customized to Pages look and feel, simply changing class name you can change color as well as its animations</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane active" id="tab3FollowUs">
+                                        <h3>
+                                            “ Nothing is <span class="semi-bold">impossible</span>, the word
+                                            itself says 'I'm <span class="semi-bold">possible</span>'! ”
+                                        </h3>
+                                        <p>
+                                            A style represents visual customizations on top of a layout. By editing a style, you can use Squarespace's visual interface to customize your...
+                                        </p>
+                                        <br>
+                                        <p class="pull-right">
+                                            <button class="btn btn-default btn-cons" type="button">White</button>
+                                            <button class="btn btn-success btn-cons" type="button">Success</button>
+                                        </p>
+                                    </div>
+                                    <div class="tab-pane" id="tab3Inspire">
+                                        <h3>
+                                            Follow us &amp; get updated!
+                                        </h3>
+                                        <p>
+                                            Instantly connect to what's most important to you. Follow your friends, experts, favorite celebrities, and breaking news.
+                                        </p>
+                                        <br>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="dropdown pull-right">
-        <button class="profile-dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">
-                <span class="thumbnail-wrapper d32 circular inline m-t-5">
-                <i class="fa fa-user" style="font-size: 20px;"></i>
-            </span>
-        </button>
-        <ul class="dropdown-menu profile-dropdown" role="menu">
-            <li><a href="/account"><i class="pg-settings_small"></i> My account</a></li>
-            <li><a href="#"><i class="pg-italic"></i> Help</a></li>
-            <li><a href="/contact"><i class="pg-mail"></i> Contact</a></li>
-            <li class="bg-master-lighter">
-                <a href="/logout" class="clearfix">
-                    <span class="pull-left">Logout</span>
-                    <span class="pull-right"><i class="pg-power"></i></span>
-                </a>
-            </li>
-        </ul>
-    </div>
-</div>
-<!-- END User Info-->
-@endif
+@endsection
