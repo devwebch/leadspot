c6e60a94374d1774803e4e871cef600e2d0534c6
Simon Rapin <simon.rapin@gmail.com>
2016-09-11 15:48:23 +0200

    Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,17 @@
 <head>
     @include('layouts.partials.head')
 </head>
-<body class="fixed-header menu-pin-nope">
+<body class="fixed-header">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-10894705-4', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <!-- BEGIN SIDEBAR -->
 <div class="page-sidebar" data-pages="sidebar">
     <!-- BEGIN SIDEBAR HEADER -->
--- resources/views/layouts/naked.blade.php
+++ resources/views/layouts/naked.blade.php
@@ -4,6 +4,16 @@
 @include('layouts.partials.head')
 </head>
 <body class="fixed-header ">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-10894705-4', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <div class="register-container full-height sm-p-t-30">
     <div class="container-sm-height full-height">
         <div class="row row-sm-height">
