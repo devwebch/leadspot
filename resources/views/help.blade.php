e0e4f2af5c1e9cbeaa80164b10199f7c90f12d47
Simon Rapin <simon.rapin@gmail.com>
2016-09-19 10:13:44 +0200

    help page

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -22,10 +22,25 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-6">
-
-                            <h4>How can I delete my account</h4>
-                            <p>Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</p>
-
+                            <div class="m-b-20">
+                                <h4>What is LeadSpot?</h4>
+                                <p>LeadSpot is an online tool created to help young agencies and freelancers to find new clients. When starting up, it can be quite difficult to find clients, LeadSpot is here to analyze the online presence of local business in a defined area.</p>
+                            </div>
+                            <div class="m-b-20">
+                                <h4>Is this a paid service?</h4>
+                                <p>LeadSpot is open to everyone through our free plan, no credit cards are required to open a free account.</p>
+                                <p>Paid subscriptions are available, unlocking advanced features and increasing your daily limits.</p>
+                            </div>
+                            <div class="m-b-20">
+                                <h4>I would like to cancel my subscriptions</h4>
+                                <p>We would be sad to see you go, but our goal is your full satisfaction regarding our services. Simply <a href="mailto:support@leadspotapp.com">get in touch with our support</a> and we will cancel your subscription. However we do not offer refunds.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="m-b-20">
+                                <h4>I need help using LeadSpot</h4>
+                                <p>Do not hesitate to <a href="mailto:support@leadspotapp.com">contact our support</a> by e-mail, we will gladly help you find what you need.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
