e78f661c5f0b97ea7d3226140e654d2065249ca4
Simon Rapin <srapin@novahealthsa.com>
2016-09-20 16:51:20 +0200

    added decrease use method

--- app/SubscriptionsUsage.php
+++ app/SubscriptionsUsage.php
@@ -8,15 +8,33 @@ class SubscriptionsUsage extends Model
 {
     protected $table = 'subscriptions_usage';
 
+    /**
+     * User related to SubscriptionUsage
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo('App\User');
     }
 
+    /**
+     * Increases the stored used count
+     */
     public function increaseUse()
     {
         if ( $this->used < $this->limit ){
             $this->used += 1;
+            $this->save();
+        }
+    }
+
+    /**
+     * Decreases the stored used count
+     */
+    public function decreaseUse()
+    {
+        if ( $this->used > 0 ){
+            $this->used -= 1;
             $this->save();
         }
     }
