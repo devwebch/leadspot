4e8c4a54641b6b05268054f0535badfa40866baf
Simon Rapin <srapin@novahealthsa.com>
2016-10-10 13:40:29 +0200

    password reset translation

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,14 @@ return [
     |
     */
 
-    'password'      => 'Passwords must be at least six characters and match the confirmation.',
-    'reset'         => 'Your password has been reset!',
-    'sent'          => 'We have e-mailed your password reset link!',
-    'token'         => 'This password reset token is invalid.',
-    'user'          => "We can't find a user with that e-mail address.",
+    'password'          => 'Passwords must be at least six characters and match the confirmation.',
+    'reset'             => 'Your password has been reset!',
+    'sent'              => 'We have e-mailed your password reset link!',
+    'token'             => 'This password reset token is invalid.',
+    'user'              => "We can't find a user with that e-mail address.",
+    'password_reset'    => 'Password reset',
+    'intro'             => 'Insert your e-mail address, you will then receive the instructions to reset your password.',
+    'email_address'     => 'E-mail address',
+    'send_password'     => 'Send Password Reset Link',
 
 ];
